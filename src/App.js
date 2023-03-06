f47c8a99fcd1ab74d22145631b2f0462d84f3d32
Mustaq Basha Shaik <mshaik@evoketechnologies.com>
2023-03-06 13:22:51 +0530

    changed the date

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Evoke Technologies - React App - 05/03/2023
+          Evoke Technologies - React App - 06/03/2023
         </p>
         <a
           className="App-link"
